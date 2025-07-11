97ede8dc3563a9ebce634e3a9707511482458d11
Adham Bouras <46635159+42-adbouras@users.noreply.github.com>
2025-07-11 15:24:36 +0000

    socket fund

--- webserv/includes/webserv.hpp
+++ webserv/includes/webserv.hpp
@@ -6,10 +6,13 @@
 /*   By: adbouras <adbouras@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 11:20:43 by adbouras          #+#    #+#             */
-/*   Updated: 2025/07/11 11:32:33 by adbouras         ###   ########.fr       */
+/*   Updated: 2025/07/11 14:55:35 by adbouras         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
