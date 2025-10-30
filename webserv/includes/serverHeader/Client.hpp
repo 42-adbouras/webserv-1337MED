26500a042567299ec72303b8e7a146a6c921d7a3
MOHAMED <mhdjant607@gmail.com>
2025-10-30 20:55:28 +0100

    UPDATE CLIENT-STATE LOGIC

--- webserv/includes/serverHeader/Client.hpp
+++ webserv/includes/serverHeader/Client.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <iostream>
+#include <ctime>
 #include <SocketManager.hpp>
+#include "ServerUtils.hpp"
 #include "../TypeDefs.hpp"
 #include "../request.hpp"
 
@@ -11,21 +12,35 @@ enum	Status {
 	NON
 };
 
+enum    ClientState {
+    CS_NEW,
+    CS_READING,
+    CS_READING_DONE,
+    CS_WRITING,
+    CS_WRITING_DONE,
+    CS_KEEPALIVE,
+    CS_DISCONNECT
+    // CS_CGI_PROCESSING // FOR CGI REQUEST
+};
+
 class   Client {
     private:
-        int     _fd;
-        Status  _status;
-		Request _request;
+        int             _fd;
+        ClientState     _clientState;
+		Request         _request;
+        std::time_t     _startTimeOut;
+    
         Client();
     public:
         serverBlockHint _serverBlockHint;
         Client(int fd, const serverBlockHint& server_block);
         ~Client();
-        int     getFd() const;
-        void    setFd(int fd);
-        Status getStatus() const;
-        void    setStatus(Status status);
-		void	setRequest( Request req );
-		Request& getRequest();
+        int         getFd() const;
+        void        setFd(int fd);
+        ClientState getStatus() const;
+        void        setTimeOut(std::time_t current);
+        void        setClientState(ClientState clientState);
+		void	    setRequest( Request req );
+		Request&    getRequest();
 
 };
