0b6879c3a90b764875123942fa07173fe815a9e9
Kl3z <78610663+abdelali77@users.noreply.github.com>
2025-11-06 18:21:19 +0000

    Response:getLocation

--- webserv/sources/response/deleteHandler.cpp
+++ webserv/sources/response/deleteHandler.cpp
@@ -1,20 +1,25 @@
 #include "../../includes/response.hpp"
 
-void deleteHandler(ServerEntry *_srvEntry, Request& request, Response& response) {
-	str source = request.getPath();
-	// std::cout << source << std::endl;
-	std::deque<str> segments = splitPath(source);
-	Location location = getLocation(_srvEntry, request);
-	if (location._allowedMethods.find(request.getMethod())
-		== location._allowedMethods.end())
-		errorResponse(response, METHOD_NOT_ALLOWED);
+void redirResponse( Response& response, Location location ) {
+	response.setStatus(location._redirCode);
+	response.addHeaders("Location", location._redirTarget);
+	response.addHeaders("Content-Length", iToString(response.getContentLength()));
+
+	response.setBody("Moved Permanently. Redirecting to " + location._redirTarget);
+}
+
+void deleteHandler(ServerEntry *_srvEntry, Request& request, Response& response, str& src) {
+	(void)src;
+	Location location = getLocation(_srvEntry, request, response);
 	if (request.getBody().length()) {
 		if (request.getBody().length() > _srvEntry->_maxBodySize)
 			errorResponse(response, CONTENET_TOO_LARGE);
+	if (location._allowedMethods.find(request.getMethod())
+		== location._allowedMethods.end())
+		errorResponse(response, METHOD_NOT_ALLOWED);
 	}
-	// std::deque<str>::iterator it = segments.begin();
-	// while(it != segments.end()) {
-	// 	std::cout << *it << std::endl;
-	// 	++it;
-	// }
+	if (location._redirSet) {
+		redirResponse(response, location);
+		return;
+	}
 }
